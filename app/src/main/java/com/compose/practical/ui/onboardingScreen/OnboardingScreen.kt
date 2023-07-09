91f005c7ba734d6706fd050a4398b905af516241
manishMedia <manishkaushik900@gmail.com>
2023-07-09 12:04:34 +0200

    Onboard Screen with Tabs

--- app/src/main/java/com/compose/practical/ui/onboardingScreen/OnboardingScreen.kt
+++ app/src/main/java/com/compose/practical/ui/onboardingScreen/OnboardingScreen.kt
@@ -32,18 +32,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.compose.practical.R
 
-val onboardPages = listOf(
-    OnboardingPage(
+val onboardPagesList = listOf(
+    OnboardPage(
         imageRes = R.drawable.image1,
         title = "Welcome to Onboarding",
         description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-    ),
-    OnboardingPage(
+    ), OnboardPage(
         imageRes = R.drawable.image2,
         title = "Explore Exciting Features",
         description = "Praesent at semper est, nec consectetur justo."
-    ),
-    OnboardingPage(
+    ), OnboardPage(
         imageRes = R.drawable.image3,
         title = "Get Started Now",
         description = "In auctor ultrices turpis at blandit."
@@ -52,101 +50,118 @@ val onboardPages = listOf(
 
 
 @Composable
-fun OnboardScreen(onboardPages: List<OnboardingPage> = emptyList()) {
+fun OnboardScreen() {
+
+    val onboardPages = onboardPagesList
 
     val currentPage = remember { mutableStateOf(0) }
 
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        /*Image(
-            painter = painterResource(id = onboardPages[currentPage.value].imageRes),
-            contentDescription = null,
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxWidth(),
-            contentScale = ContentScale.FillWidth
-        )
-*/
-        ImageWithBottomFade(
+
+        OnBoardImageView(
             modifier = Modifier
                 .weight(1f)
                 .fillMaxWidth(),
             imageRes = onboardPages[currentPage.value].imageRes
         )
 
-        Column(
+        OnBoardDetails(
             modifier = Modifier
                 .weight(1f)
-                .padding(16.dp)
-        ) {
-            Text(
-                text = onboardPages[currentPage.value].title,
-                style = MaterialTheme.typography.displaySmall,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = onboardPages[currentPage.value].description,
-                style = MaterialTheme.typography.bodyMedium,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth()
-            )
-        }
-        Button(
-            onClick = {
-                if (currentPage.value < onboardPages.size - 1) {
-                    currentPage.value++
-                } else {
-                    // Handle onboarding completion
-                }
-            },
+                .padding(16.dp),
+            currentPage = onboardPages[currentPage.value]
+        )
+
+        OnBoardNavButton(
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
-                .padding(top = 16.dp)
+                .padding(top = 16.dp),
+            currentPage = currentPage.value,
+            noOfPages = onboardPages.size
         ) {
-            Text(text = if (currentPage.value < onboardPages.size - 1) "Next" else "Get Started")
+            currentPage.value++
         }
 
-        TabSelector(currentPage = currentPage.value) { index ->
+        TabSelector(
+            onboardPages = onboardPages,
+            currentPage = currentPage.value
+        ) { index ->
             currentPage.value = index
         }
     }
 }
 
+@Composable
+fun OnBoardDetails(
+    modifier: Modifier = Modifier, currentPage: OnboardPage
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = currentPage.title,
+            style = MaterialTheme.typography.displaySmall,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = currentPage.description,
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
 
 @Composable
-fun ImageWithBottomFade(modifier: Modifier = Modifier, imageRes: Int) {
-    Box(modifier =modifier) {
+fun OnBoardNavButton(
+    modifier: Modifier = Modifier, currentPage: Int, noOfPages: Int, onNextClicked: () -> Unit
+) {
+    Button(
+        onClick = {
+            if (currentPage < noOfPages - 1) {
+                onNextClicked()
+            } else {
+                // Handle onboarding completion
+            }
+        }, modifier = modifier
+    ) {
+        Text(text = if (currentPage < noOfPages - 1) "Next" else "Get Started")
+    }
+}
+
+
+@Composable
+fun OnBoardImageView(modifier: Modifier = Modifier, imageRes: Int) {
+    Box(modifier = modifier) {
         Image(
             painter = painterResource(id = imageRes),
             contentDescription = null,
             modifier = Modifier.fillMaxSize(),
             contentScale = ContentScale.FillWidth
         )
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .align(Alignment.BottomCenter)
-                .graphicsLayer {
-                    // Apply alpha to create the fading effect
-                    alpha = 0.6f
-                }
-                .background(
-                    Brush.verticalGradient(
-                        colorStops = arrayOf(
-                            Pair(0.8f, Color.Transparent),
-                            Pair(1f, Color.White)
-                        )
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .align(Alignment.BottomCenter)
+            .graphicsLayer {
+                // Apply alpha to create the fading effect
+                alpha = 0.6f
+            }
+            .background(
+                Brush.verticalGradient(
+                    colorStops = arrayOf(
+                        Pair(0.8f, Color.Transparent), Pair(1f, Color.White)
                     )
                 )
-        )
+            ))
     }
 }
 
 @Composable
-fun TabSelector(currentPage: Int, onTabSelected: (Int) -> Unit) {
+fun TabSelector(onboardPages: List<OnboardPage>, currentPage: Int, onTabSelected: (Int) -> Unit) {
     TabRow(
         selectedTabIndex = currentPage,
         modifier = Modifier
@@ -154,40 +169,32 @@ fun TabSelector(currentPage: Int, onTabSelected: (Int) -> Unit) {
             .background(MaterialTheme.colorScheme.primary)
     ) {
         onboardPages.forEachIndexed { index, _ ->
-            Tab(
-                selected = index == currentPage,
-                onClick = {
-                    onTabSelected(index)
-                },
-                modifier = Modifier.padding(16.dp),
-                content = {
-                    Box(
-                        modifier = Modifier
-                            .size(8.dp)
-                            .background(
-                                color = if (index == currentPage) MaterialTheme.colorScheme.onPrimary
-                                else Color.LightGray,
-                                shape = RoundedCornerShape(4.dp)
-                            )
-                    )
-                }
-            )
+            Tab(selected = index == currentPage, onClick = {
+                onTabSelected(index)
+            }, modifier = Modifier.padding(16.dp), content = {
+                Box(
+                    modifier = Modifier
+                        .size(8.dp)
+                        .background(
+                            color = if (index == currentPage) MaterialTheme.colorScheme.onPrimary
+                            else Color.LightGray, shape = RoundedCornerShape(4.dp)
+                        )
+                )
+            })
         }
     }
 }
 
-data class OnboardingPage(
-    val imageRes: Int,
-    val title: String,
-    val description: String
+data class OnboardPage(
+    val imageRes: Int, val title: String, val description: String
 )
 
 @Preview
 @Composable
-fun PreviewOnboardingScreen() {
+fun PreviewOnboardScreen() {
     MaterialTheme {
         Surface {
-            OnboardScreen(onboardPages)
+            OnboardScreen()
         }
     }
 
